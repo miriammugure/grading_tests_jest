b0e5023cfef1765b9f7a2cb312d981a8fcc5196a
miriam <miriammugure610@gmail.com>
2024-05-30 18:56:59 +0300

    add the grading test

--- main.test.js
+++ main.test.js
@@ -1,5 +1,5 @@
 import {humanities,sciences,languages,calculateGradeAverage} from "./main";
-describe("humanities", () =>{
+describe("grading", () =>{
     it ("returns A if the marks is above 80 to 100" ,() =>{
         expect(humanities(80)).toBe('A');
     })
@@ -56,21 +56,23 @@ describe("languages", () =>{
     })
 
     describe("calculateGradeAverage", () =>{
-        it ("returns A if the marks is above 80 to 100" ,() =>{
-            expect(humanities(80)).toBe('A');
+        it ("returns the name of the student ,grades and the average" ,() =>{
+            const studentScore ={name:"Miriam Mugure",math:80,eng:65,kis:48,phy:67,che:56,re:89,comp:82
+              };
+              const result = calculateGradeAverage(studentScore);
+              expect(result).toEqual({
+                  name: "Miriam Mugure",
+                  math: "A",
+                  eng: "B",
+                  kis: "Pass",
+                  phy: "B",
+                  che: "C",
+                  re: "A",
+                  comp: "A",
+                  average: 69
+              });
         })
-        it ("returns B if the marks is above 70 to 79" ,() =>{
-            expect(humanities(75)).toBe('B');
-        })
-        it ("returns c if the marks is above 60 to 69" ,() =>{
-            expect(humanities(65)).toBe('C');
-        })
-        it ("returns Pass if the marks is above 50 to 59" ,() =>{
-            expect(humanities(55)).toBe('Pass');
-        })
-        it ("returns Sup if the marks is below 50" ,() =>{
-            expect(humanities(49)).toBe('Sup');
-        })
+       
     
     
     })
